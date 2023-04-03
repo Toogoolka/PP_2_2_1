9f9c4a0aab472b5d6a6746a6b7e6acdba2d05ebe
Toogoolka <120179327+Toogoolka@users.noreply.github.com>
2023-04-03 12:12:37 +0300

    repair DI

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -1,84 +1,90 @@
 package hiber.model;
 
-import javax.persistence.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import javax.persistence.*;
+@Component
 @Entity
 @Table(name = "users")
 public class User {
 
-   @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
-   private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-   @Column(name = "name")
-   private String firstName;
+    @Column(name = "name")
+    private String firstName;
 
-   @Column(name = "last_name")
-   private String lastName;
+    @Column(name = "last_name")
+    private String lastName;
 
-   @Column(name = "email")
-   private String email;
+    @Column(name = "email")
+    private String email;
+    @OneToOne(cascade = CascadeType.ALL)
+    private Car car;
 
+    @Autowired
+    public User(Car car) {
+        this.car = car;
+    }
 
+    public User() {
+    }
 
-   @OneToOne(cascade = CascadeType.ALL)
-   private Car car;
+    @Override
+    public String toString() {
+        return "User {" +
+                "id = " + id +
+                ", firstName = '" + firstName + '\'' +
+                ", lastName = '" + lastName + '\'' +
+                ", email = '" + email + '\'' +
+                '}';
+    }
 
-   public User() {}
+    public User(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
 
-   @Override
-   public String toString() {
-      return "User {" +
-              "id = " + id +
-              ", firstName = '" + firstName + '\'' +
-              ", lastName = '" + lastName + '\'' +
-              ", email = '" + email + '\'' +
-              '}';
-   }
+    public Long getId() {
+        return id;
+    }
 
-   public User(String firstName, String lastName, String email) {
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.email = email;
-   }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-   public Long getId() {
-      return id;
-   }
+    public String getFirstName() {
+        return firstName;
+    }
 
-   public void setId(Long id) {
-      this.id = id;
-   }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-   public String getFirstName() {
-      return firstName;
-   }
+    public String getLastName() {
+        return lastName;
+    }
 
-   public void setFirstName(String firstName) {
-      this.firstName = firstName;
-   }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-   public String getLastName() {
-      return lastName;
-   }
+    public String getEmail() {
+        return email;
+    }
 
-   public void setLastName(String lastName) {
-      this.lastName = lastName;
-   }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-   public String getEmail() {
-      return email;
-   }
+    public Car getCar() {
+        return car;
+    }
 
-   public void setEmail(String email) {
-      this.email = email;
-   }
-
-   public Car getCar() {
-      return car;
-   }
-
-   public void setCar(Car car) {
-      this.car = car;
-   }
+    public void setCar(Car car) {
+        this.car = car;
+    }
 }
